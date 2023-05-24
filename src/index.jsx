340385eec7eae35894283ebb23f9fb3950160af2
jefferychai <122602765+jefferychai@users.noreply.github.com>
2023-05-24 16:08:12 +0800

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./index.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VANLIFE
+        </Link>
+        <nav>
+          <Link to="/About">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
